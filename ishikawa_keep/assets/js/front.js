0eaabb41ac91bf60c1302c3b00e621880d58a012
ishikawa-saori <mawe.na.mito.design@gmail.com>
2022-02-27 22:52:25 +0900

    commom.cssの修正、front.jsの修正。ドロワーメニュー内が変なのは直った（と思う）背景固定が１回しか発動しない…

--- ishikawa_keep/assets/js/front.js
+++ ishikawa_keep/assets/js/front.js
@@ -2,40 +2,51 @@
 
 // ▼ハンバーガーメニュー 表示切り替え▼
 $(function () {
-  $(".header_wrap_box_nav").on("click", function () {
-    $(this).children().toggleClass("active");
-  });
+    $(".header_wrap_box_nav").on("click", function () {
+        $(this).children().toggleClass("active");
+    });
 });
 
-// ▼ドロワーメニュー開閉▼
+// ▼ドロワーメニュー開閉/背景固定▼
+var state = false;
+var scrollpos;
 $(function () {
-  $(".header_wrap_box_nav").on("click", function () {
-    $(".drower").toggleClass("visible");
-  });
+    $(".header_wrap_box_nav").on("click", function () {
+        $(".drower").toggleClass("visible");
+        if (state == false) {
+            scrollpos = $(window).scrollTop();
+            $("body").addClass("fixed").css({ top: -scrollpos });
+            state = true;
+        } else {
+            $("body").removeClass("fixed").css({ top: 0 });
+            window.scrollTop(0, scrollpos);
+            state = false;
+        }
+    });
 });
 
 // ▼店舗情報セクション／タブ切り替え▼
 function GethashID(hashIDName) {
-  if (hashIDName) {
-    $(".tab_wrap li")
-      .find("a")
-      .each(function () {
-        let idName = $(this).attr("href");
-        if (idName == hashIDName) {
-          let parentElm = $(this).parent();
-          $(".tab_wrap li").removeClass("active");
-          $(parentElm).addClass("active");
-          $(".area").removeClass("is_active");
-          $(hashIDName).addClass("is_active");
-        }
-      });
-  }
+    if (hashIDName) {
+        $(".tab_wrap li")
+            .find("a")
+            .each(function () {
+                let idName = $(this).attr("href");
+                if (idName == hashIDName) {
+                    let parentElm = $(this).parent();
+                    $(".tab_wrap li").removeClass("active");
+                    $(parentElm).addClass("active");
+                    $(".area").removeClass("is_active");
+                    $(hashIDName).addClass("is_active");
+                }
+            });
+    }
 }
 
 $(".tab_item").on("click", function () {
-  let idName = $(this).attr("href");
-  GethashID(idName);
-  return false;
+    let idName = $(this).attr("href");
+    GethashID(idName);
+    return false;
 });
 
 // ページを読み込んだとき（初期状態）は「飲む」タブとエリアを表示
@@ -48,29 +59,29 @@ $(".tab_item").on("click", function () {
 
 // scroolイベント
 $(function () {
-  let main_h = $(".mainvisual_wrap").outerHeight();
-  $(window).scroll(function () {
-    var scr = $(this).scrollTop();
-    if (scr > main_h) {
-      $(".header_nav_list").css({ color: "#542912" });
-    } else {
-      $(".header_nav_list").css({ color: "#fff" });
-    }
-  });
+    let main_h = $(".mainvisual_wrap").outerHeight();
+    $(window).scroll(function () {
+        var scr = $(this).scrollTop();
+        if (scr > main_h) {
+            $(".header_nav_list").css({ color: "#542912" });
+        } else {
+            $(".header_nav_list").css({ color: "#fff" });
+        }
+    });
 });
 
 $(function () {
-  let bottom_h = $(".bottom_box").outerHeight();
-  let height = $(window).height();
-  let footer_h = $("#footer_wrap").offset().top;
-  $(window).scroll(function () {
-    var scr = $(this).scrollTop();
-    if (scr > footer_h - height) {
-      $(".bottom_box").css("opacity", 0);
-    } else if (scr > bottom_h) {
-      $(".bottom_box").css("opacity", 1);
-    } else {
-      $(".bottom_box").css("opacity", 0);
-    }
-  });
+    let bottom_h = $(".bottom_box").outerHeight();
+    let height = $(window).height();
+    let footer_h = $("#footer_wrap").offset().top;
+    $(window).scroll(function () {
+        var scr = $(this).scrollTop();
+        if (scr > footer_h - height) {
+            $(".bottom_box").css("opacity", 0);
+        } else if (scr > bottom_h) {
+            $(".bottom_box").css("opacity", 1);
+        } else {
+            $(".bottom_box").css("opacity", 0);
+        }
+    });
 });
