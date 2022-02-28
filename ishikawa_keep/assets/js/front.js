29775b6696876d90e463c58abf5853f54433f908
ishikawa-saori <mawe.na.mito.design@gmail.com>
2022-02-28 15:52:56 +0900

    ドロワーメニュー背景固定：front.js変更

--- ishikawa_keep/assets/js/front.js
+++ ishikawa_keep/assets/js/front.js
@@ -2,51 +2,42 @@
 
 // ▼ハンバーガーメニュー 表示切り替え▼
 $(function () {
-  $(".header_wrap_box_nav").on("click", function () {
-    $(this).children().toggleClass("active");
-  });
-});
-
-// ▼ドロワーメニュー開閉/背景固定▼
-var state = false;
-var scrollpos;
-$(function () {
-  $(".header_wrap_box_nav").on("click", function () {
-    $(".drower").toggleClass("visible");
-    if (state == false) {
-      scrollpos = $(window).scrollTop();
-      $("body").addClass("fixed").css({ top: -scrollpos });
-      state = true;
-    } else {
-      $("body").removeClass("fixed").css({ top: 0 });
-      window.scrollTop(0, scrollpos);
-      state = false;
-    }
-  });
+    $(".header_wrap_box_nav").click(function () {
+        $(this).toggleClass("active");
+        if ($(".header_wrap_box_nav").hasClass("active")) {
+            $(".drower").addClass("visible");
+            scrollpos = $(window).scrollTop();
+            $("body").addClass("fixed").css({ top: -scrollpos });
+        } else {
+            $(".drower").removeClass("visible");
+            $("body").removeClass("fixed").css({ top: 0 });
+            window.scrollTop(0, scrollpos);
+        }
+    });
 });
 
 // ▼店舗情報セクション／タブ切り替え▼
 function GethashID(hashIDName) {
-  if (hashIDName) {
-    $(".tab_wrap li")
-      .find("a")
-      .each(function () {
-        let idName = $(this).attr("href");
-        if (idName == hashIDName) {
-          let parentElm = $(this).parent();
-          $(".tab_wrap li").removeClass("active");
-          $(parentElm).addClass("active");
-          $(".area").removeClass("is_active");
-          $(hashIDName).addClass("is_active");
-        }
-      });
-  }
+    if (hashIDName) {
+        $(".tab_wrap li")
+            .find("a")
+            .each(function () {
+                let idName = $(this).attr("href");
+                if (idName == hashIDName) {
+                    let parentElm = $(this).parent();
+                    $(".tab_wrap li").removeClass("active");
+                    $(parentElm).addClass("active");
+                    $(".area").removeClass("is_active");
+                    $(hashIDName).addClass("is_active");
+                }
+            });
+    }
 }
 
 $(".tab_item").on("click", function () {
-  let idName = $(this).attr("href");
-  GethashID(idName);
-  return false;
+    let idName = $(this).attr("href");
+    GethashID(idName);
+    return false;
 });
 
 // ページを読み込んだとき（初期状態）は「飲む」タブとエリアを表示
@@ -65,29 +56,29 @@ let gnav_h = $(".header_wrap_box").outerHeight();
 
 // scroolイベント詳細(gnav_appearance)
 $(window).on("scroll resize", function () {
-  var main_h = $(".mainvisual_wrap").outerHeight();
-  var footer_h = $("#footer_wrap").offset().top;
-  var scr = $(this).scrollTop();
-  if (scr > footer_h - gnav_h / 2) {
-    $(".header_nav_list").hide();
-  } else if (scr > main_h - gnav_h / 2) {
-    $(".header_nav_list").show();
-    $(".header_nav_list").css({ color: "#542912" });
-  } else {
-    $(".header_nav_list").css({ color: "#fff" });
-  }
+    var main_h = $(".mainvisual_wrap").outerHeight();
+    var footer_h = $("#footer_wrap").offset().top;
+    var scr = $(this).scrollTop();
+    if (scr > footer_h - gnav_h / 2) {
+        $(".header_nav_list").hide();
+    } else if (scr > main_h - gnav_h / 2) {
+        $(".header_nav_list").show();
+        $(".header_nav_list").css({ color: "#542912" });
+    } else {
+        $(".header_nav_list").css({ color: "#fff" });
+    }
 });
 
 // scroolイベント詳細(selectbtn_appearance)
 $(window).on("scroll resize", function () {
-  var height = $(window).height();
-  var footer_h = $("#footer_wrap").offset().top;
-  var scr = $(this).scrollTop();
-  if (scr > footer_h - height) {
-    $(".bottom_box").css("opacity", 0);
-  } else if (scr > selectbtn_h) {
-    $(".bottom_box").css("opacity", 1);
-  } else {
-    $(".bottom_box").css("opacity", 0);
-  }
+    var height = $(window).height();
+    var footer_h = $("#footer_wrap").offset().top;
+    var scr = $(this).scrollTop();
+    if (scr > footer_h - height) {
+        $(".bottom_box").css("opacity", 0);
+    } else if (scr > selectbtn_h) {
+        $(".bottom_box").css("opacity", 1);
+    } else {
+        $(".bottom_box").css("opacity", 0);
+    }
 });
